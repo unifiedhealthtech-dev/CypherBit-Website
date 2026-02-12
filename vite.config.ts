b3374068f29559dae60436ab4ac685c3f09d991d
unifiedhealthtech-dev <unifiedhealthtech@gmail.com>
2026-02-13 00:03:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
+  base: "/CypherBit-Website/",   // ⭐ required for GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
